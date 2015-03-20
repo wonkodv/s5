8c425abaa3297d92243a32f570f939ff4744fd11
wonko <wonko@hanstool.org>
2015-03-20 19:52:27 +0100

    Setup: ensure py3.4, fallback to distutils W I P
    distutils not tested
    egg propably defeats plugins

--- setup.py
+++ setup.py
@@ -1,12 +1,30 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3.4
+import sys
 
-from setuptools import setup
+if sys.version_info[:2] < (3, 4):
+    print("Minimal python version is 3.4")
 
-setup(
+
+params = dict(
     name='s5',
     description='Secure Self hosted Synchronization and Sharing Service (S5)',
-    author='Matthias Riegel',
+    author='wonko@hanstool.org',
     version='0.0',
     packages=['s5'],
-    entry_points={'console_scripts': ['s5server = s5.server', 's5 = s5.client']}
 )
+
+try:
+    from setuptools import setup
+    setup(
+        install_requires=['pycrypto'],
+        entry_points={
+            'console_scripts': ['s5server = s5.server', 's5 = s5.client']
+        },
+        ** params
+    )
+except Exception as e:
+    raise e
+    from distutils.core import setup
+    setup(**params)
+    print("Please ensure you have `pycrypto` installed and create scripts"
+          " in your PATH that execute `s5.client` and `s5.server`")
